db23e8f23759c4633daa1ae0b3a25f34380920c5
illyasviell <ashja234@gmail.com>
2020-06-07 20:01:08 +0700

    some fix

--- src/index.js
+++ src/index.js
@@ -5,45 +5,105 @@ import usersPost from "./api/userpost"
 import bodyParser from "body-parser"
 import checkUser from './api/checkuser'
 import path from 'path'
-import mongodb from 'mongodb'
 import fileUpload from 'express-fileupload'
 import morgan from 'morgan'
 import _ from 'lodash'
+import multer from 'multer'
 import health from 'express-ping'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import io from 'socket.io-client'
 
+const server = {
+  "azure": "http://sl-azure.herokuapp.com",
+  "crimson": "http://sl-crimson.herokuapp.com",
+  "firebrick": "http://sl-firebrick.herokuapp.com",
+  "magenta": "http://sl-magenta.herokuapp.com",
+  "navy": "http://sl-navy.herokuapp.com",
+  "linen": "http://sl-linen.herokuapp.com",
+  "thistle": "http://sl-thistle.herokuapp.com",
+  "aliceblue": "http://sl-aliceblue.herokuapp.com",
+  "sienna": "http://sl-sienna.herokuapp.com",
+  "ivory": "http://sl-ivory.herokuapp.com"
+}
+const ServerSockets = {
+  navy: io(server["navy"]),
+  azure: io(server["azure"]),
+  crimson: io(server["crimson"]),
+  firebrick: io(server["firebrick"]),
+  magenta: io(server["magenta"])
+}
 
-const MongoClient = mongodb.MongoClient
-const uri = "mongodb+srv://SlimeDev:a1s2h3j4a5@cluster0-qnvfk.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected to mongo database")
-  // perform actions on the collection object;;
-  client.close();
-});
+const alfabet = ["a", "2", "e", "1", "f", "4", "d", "5", "c", "6", "a", "9", "d", "0", "b", "3", "6", "c", "8", "c", "4", "c", "1", "b", "a", "5", "e", "3", "1", "5", "6", "7", "1", "4", "5", "b", "e", "f", "f", "f", "1", "3", "4", "7", "9", "a", "e", "1", "d", "b", "2", "6", "b", "7", "7"]
+let users = {
 
-
+}
+setInterval(() => {
+  const time = `${new Date().getFullYear() * new Date().getSeconds()}${new Date().getDate() * new Date().getSeconds()}${new Date().getHours() * new Date().getSeconds()}${new Date().getMinutes() * new Date().getSeconds()}${new Date().getSeconds() * new Date().getSeconds()}`
+  let token = ""
+  time.split("").forEach(t => {
+    token = token + alfabet[t]
+  })
+  fetch(server["aliceblue"] + "/user?token=" + token)
+    .then((data) => data.json())
+    .then(data => {
+      users = data
+    })
+    .catch(e => { })
+}, 10)
 let x = express()
 let http = require("http").createServer(x)
-var io = require("socket.io")(http)
-
-const PORT = process.env.PORT || 3000
+var ion = require("socket.io")(http, { wsEngine: "ws" })
+const PORT = process.env.PORT || 7002
 
 x.use(cors())
 x.use(session({ secret: "nodirectaccess" }))
 x.use(bodyParser({ limit: "10mb" }))
+x.use(bodyParser.urlencoded());
+x.use(bodyParser.urlencoded({ extended: true }));
 x.use(fileUpload({ createParentPath: true }));
 x.use(morgan('dev'));
-x.use(health.ping())
+x.use(health.ping());
+health.info((data) => { console.log(data) })
 
+
+const Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './img')
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage: Storage }).single('file')
+x.post('/files', (req, res) => {
+  upload(req, res, err => {
+    if (err) console.error(err)
+    const file = req.file
+    const meta = req.body
+
+    console.log(file)
+    console.log(meta)
+  })
+})
 /**
  * index, not really important tho
  */
 x.all("/", (req, res) => {
   res.send("Slimechat API V.0.2 Beta --- NO DIRECT ACCESS ALLOWED")
-  console.log(req.query.name)
 })
 
+x.post("/up", (req, res) => {
+  const filename = req.body.filename
+  const file = req.body.file
+  var wstream = fs.createWriteStream(__dirname + "/../img/" + filename, { encoding: 'base64' });
+  wstream.write(file);
+  wstream.end();
+})
+x.post("/uploadFile", (req, res) => {
+  console.log(req.body)
+})
 /**
  * Status handler, require api key and name query
  * Only get and post. After certain time the status will be deleted
@@ -53,7 +113,7 @@ x.all("/", (req, res) => {
  * it expect contactset query
  * return status thumbnail and state
  */
-x.get("/status", (req, res)=>{
+x.get("/status", (req, res) => {
   let data = req.body.data
   data = JSON.parse(data)
   if (!Array.isArray(data)) res.send("404")
@@ -65,11 +125,11 @@ x.get("/status", (req, res)=>{
  * return mp4 for video
  * return mp3 for audio
  */
-x.get("/status/:name", (req, res)=>{
+x.get("/status/:name", (req, res) => {
 
 })
-x.post("/status", (req, res)=>{
-  
+x.post("/status", (req, res) => {
+
 })
 /**
  * img handler. img received from /user post endpoint or daftar activity
@@ -94,7 +154,7 @@ x.get("/img/:name", (req, res) => {
  *  nomor : String
  *  avatar : String(base64)
  *  frontName : String
- *  status : String //it should be bio
+ *  status : String //it should be bion
  * }) 
  *
  */
@@ -104,36 +164,87 @@ x.get("/test", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 let numberOFConnectedClient = 0
-x.get("/cn", (req, res)=>{
+let onlineUser = {}
+let clientData = {}
+x.get("/cn", (req, res) => {
   res.send(numberOFConnectedClient.toString())
 })
-io.on("connection", function (socket) {
+ion.on("connection", function (socket) {
   numberOFConnectedClient++
-  socket.on("disconnect", ()=>{
-    numberOFConnectedClient--
+  const bomb = setTimeout(() => {
+    if (clientData[socket.id] == undefined) {
+      console.log(`${socket.id} is kicked`)
+      socket.disconnect()
+    } else {
+      console.log("something bad happened")
+    }
+  }, 30000)
+  socket.on("disconnect", () => {
+    try {
+      numberOFConnectedClient--
+      clientData[socket.id].online = false
+      onlineUser[clientData[socket.id].username] = false
+    } catch (e) {
+      console.log("disconnect faily")
+    }
+  })
+  socket.on("username", (username) => {
+    clientData[socket.id] = {
+      username: username,
+      online: true
+    }
+    onlineUser[username] = clientData[socket.id].online
+    clearTimeout(bomb)
   })
   try {
     console.log("a user connected")
-    console.log(socket.client.id)
-    socket.on("username", (username) => {
-      socket.client.username = username
-    })
-    socket.on("disconnect", () => {
-    })
-    socket.on("chat", function (msg) {
-      try{
 
-        const data = JSON.parse(msg)
-        console.log(msg)
-        const d = {
-          message: data.msg,
-          date: data.date,
-          sender: data.sender,
-          time: data.time
+    socket.on("chat", function (data, callback) {
+      console.log(onlineUser)
+      try {
+        const msg = JSON.parse(data)
+        console.log(msg.receiver)
+        console.log(onlineUser[msg.receiver])
+        if (onlineUser[msg.receiver] == true) {
+          const data = msg
+          console.log(msg)
+          const d = {
+            message: data.msg,
+            date: data.date,
+            sender: data.sender,
+            time: data.time
+          }
+          socket.broadcast.emit(data.receiver, JSON.stringify(d))
+          callback("delivered")
         }
-        socket.broadcast.emit(data.receiver, JSON.stringify(d))
-      }catch(e){
-        
+        else {
+          if (users[msg.receiver] != undefined) {
+            switch (users[msg.receiver]) {
+              case server["azure"]:
+                ServerSockets.azure.emit("chat", data)
+                break
+              case server["firebrick"]:
+                ServerSockets.firebrick.emit("chat", data)
+                break
+              case server["crimson"]:
+                ServerSockets.crimson.emit("chat", data)
+                break
+              case server["magenta"]:
+                ServerSockets.magenta.emit("chat", data)
+                break
+              default:
+                break;
+            }
+          }
+          console.log("forwarded to navy")
+          SocketNavy.emit("store this chat please", { type: "object", data: msg })
+          callback("sent")
+        }
+      } catch (e) {
+        console.log("failed to send msg\nproblem : " + e)
+        try {
+          callback("failed")
+        } catch (e) { }
       }
     })
     socket.on("+6285710251303", (msg) => {
